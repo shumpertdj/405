ebc6b9b6b40d07e66214774399c525b435ae490a
shumpertdj <shumpertdj@yahoo.com>
2015-03-28 10:19:14 -0700

    Static

---
+++ static/main.js
@@ -0,0 +1,52 @@
+//Darnell Shumpert
+
+var http = require('http');
+var domain = require('domain');
+var message = require('./message');
+var st = require('st');
+var mount;
+
+
+function replyError(res) {
+  try {
+    res.writeHead(500);
+    res.end('Server error.');
+  } catch (err) {
+    console.error('Error sending response with code 500.');
+  }
+};
+
+function replyNotFound(res) {
+  res.writeHead(404);
+  res.end('not found');
+}
+
+function handleRequest(req, res) {
+  console.log('Handling request for ' + req.url);
+  if (req.url === '/message'){
+    message.handle(req, res);
+  }else{
+    mount(req, res);
+  }
+}
+
+function redirectIndex(res){
+   res.writeHead(302, {
+      'Location' : '/index.html'
+      });
+   res.end();
+}
+
+var server = http.createServer();
+
+server.on('request', function(req, res) {
+  mount = st({ path: __dirname + '/static', url: '/', index: 'index.html' });
+  var d = domain.create();
+  d.on('error', function(err) {
+    console.error(req.url, err.message);
+    replyError(res);
+  });
+  d.run(function() { handleRequest(req, res); });
+});
+
+server.listen(5000);
